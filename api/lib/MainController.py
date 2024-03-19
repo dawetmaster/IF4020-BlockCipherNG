7be472cd9419c4fc837935db6f57a494c4ef8a5c
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-03-19 10:15:50 +0700

    feat: add controller di backend

--- api/lib/MainController.py
+++ api/lib/MainController.py
@@ -8,29 +8,10 @@ def register_controllers(app:Flask):
   def main():
     return "Connected"
   
-  # @app.route('/analyze',methods=["POST"])
-  # def analyze_code():
-  #   if "files" not in request.files:
-  #     flash("No file received")
-  #     return redirect('/')
-  #   archive = request.files['files']
-  #   if(not archive):
-  #     flash("No file received")
-  #     return redirect('/')
-  #   # pastikan file nya boleh diunggah
-  #   if(not is_file_allowed(archive.filename)):
-  #     flash("File not supported")
-  #     return redirect('/')
-  #   # rename
-  #   names = archive.filename.rsplit('.',1)
-  #   filename = f"{secure_filename(names[0])}-{str(int(time.time()))}.{names[1]}"
-  #   # simpan dulu    
-  #   archive_path = os.path.join(app.config['UPLOAD_PATH'],filename)
-  #   archive.save(archive_path)
-  #   # parse file
-  #   parser = FileParser(archive_path)
-  #   parser.load()
-  #   return "TBD"
+  @app.route('/encrypt', methods=["POST"])
+  def encrypt():
+    pass
   
-  # def is_file_allowed(filename:str):
-  #   return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSION
+  @app.route('/decrypt', methods=["POST"])
+  def decrypt():
+    pass
